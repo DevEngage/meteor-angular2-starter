46da153090ad3cb665be42b6e6fdd69ab7153f19
James Brinkerhoff <th3brink@gmail.com>
2016-06-26 14:47:21 -0600

    added bootstrap4

--- client/app.ts
+++ client/app.ts
@@ -2,11 +2,29 @@ import 'reflect-metadata';
 import 'zone.js/dist/zone';
 import { Component } from '@angular/core';
 import { bootstrap } from '@angular/platform-browser-dynamic';
+import {CORE_DIRECTIVES} from '@angular/common';
+import {DROPDOWN_DIRECTIVES} from 'ng2-bootstrap';
 
 @Component({
   selector: 'app',
-  templateUrl: '/client/app.html'
+  templateUrl: '/client/app.html',
+  directives: [DROPDOWN_DIRECTIVES, CORE_DIRECTIVES]
 })
-class Socially { }
+class Socially { 
+  public disabled:boolean = false;
+  public status:{isopen:boolean} = {isopen: false};
+  public items:Array<string> = ['The first choice!',
+    'And another choice for you.', 'but wait! A third!'];
+
+  public toggled(open:boolean):void {
+    console.log('Dropdown is now: ', open);
+  }
+
+  public toggleDropdown($event:MouseEvent):void {
+    $event.preventDefault();
+    $event.stopPropagation();
+    this.status.isopen = !this.status.isopen;
+  }
+}
 
 bootstrap(Socially);
